9dd7814726827cdf6c7ffab69b5a137e761bfd0e
amrmaher <amrmaher40@gmail.com>
2022-01-13 02:13:10 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './assets/sass/style.scss';
+import 'normalize.css/normalize.css';
+
+console.log("hello from webpack");
+
+function studentname(firstName, lastName){
+    console.log(firstName+ " "+ lastName);
+}
+
+studentname ("Noor", "Hill");
